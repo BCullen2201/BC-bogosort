4a800bddc76d96b3a736f747f8d71987de1a4c6b
Brandon Cullen <brandoncullen2201@gmail.com>
2022-05-12 23:37:27 -0400

    if __name__ == "__main__"

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 from os import system
 from random import shuffle
 
-listOfNumbers = [9,4,7,2,1,5,3,6,8,10]
-attempts = 1 # Lets user know how many times it took to sort listOfNumbers
+def main():
+    listOfNumbers = [9,4,7,2,1,5,3,6,8,10]
+    global attempts # lets user know how many times it took to sort listOfNumbers
+    attempts = 1
 
-sortedNumbers = listOfNumbers.copy()
-sortedNumbers.sort()
+    sortedNumbers = listOfNumbers.copy()
+    sortedNumbers.sort()
 
-def checkIfSorted():
-    global attempts
+    def checkIfSorted():
+        global attempts # how the fuck do globals work
 
-    if listOfNumbers != sortedNumbers:
-        print(listOfNumbers)
-        print(sortedNumbers)
-        print("Not sorted")
-        attempts = attempts + 1
-        print(f"Attempts: {attempts}")
-        shuffle(listOfNumbers)
-    else:
-        print(listOfNumbers)
-        print(sortedNumbers)
-        print("Numbers are sorted")
-        print(f"Attempts: {attempts}")
-        input("Press ENTER to go again") # stops infinite loop when listOfNumbers is sorted
-        shuffle(listOfNumbers)
+        if listOfNumbers != sortedNumbers:
+            print(listOfNumbers)
+            print(sortedNumbers)
+            print("Not sorted")
+            attempts = attempts + 1
+            print(f"Attempts: {attempts}")
+            shuffle(listOfNumbers)
+        else:
+            print(listOfNumbers)
+            print(sortedNumbers)
+            print("Numbers are sorted")
+            print(f"Attempts: {attempts}")
+            input("Press ENTER to go again") # stops infinite loop when listOfNumbers is sorted
+            shuffle(listOfNumbers)
 
-while True: # keeps checkIfSorted() running
-    system("clear") # keeps window clean, results are more apparent
-    checkIfSorted()
+    while True:
+        system("clear")
+        checkIfSorted()
+
+if __name__ == "__main__":
+    main()
