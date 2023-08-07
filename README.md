1d86a050915b03cf2ca83aa6328e4cc0e85b8dfc
Brandon Cullen <brandoncullen2201@gmail.com>
2023-08-07 14:50:02 -0400

    Rewrote most of script, works faster

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # BC-bogosort
 Took a shot at creating the amazing bogosort in Python3
 
-# It actually sorted a list lmao
-
-![Screenshot from 2022-05-23 17-18-15](https://user-images.githubusercontent.com/94326161/169907789-d4b746bf-465a-4ff8-8b35-bff7e845f42a.png)
+## Rewrote most of the script
+Should work much faster now
---
+++ src/main.py
@@ -0,0 +1,48 @@
+from os import system
+from random import shuffle
+from time import process_time_ns as timer
+
+def makeList(userChoice):
+    numList = []
+    userChoice = userChoice + 1
+    
+    for i in range(1,userChoice):
+        numList.append(i)
+
+    shuffle(numList)
+
+    return numList
+
+def checkIfSorted(numList):
+    system("clear")
+    print('"Sorting"...')
+    attempts = 0
+    walls = 1
+    length = len(numList) - 1
+
+    while walls != 0:
+        attempts = attempts + 1
+        walls = 0
+
+        # print(f"List:     {numList}")
+        # print(f"Attempts: {attempts}")
+
+        for i in range(0, length):
+            if numList[i] > numList[i + 1]:
+                walls = walls + 1
+                shuffle(numList)
+
+    system("clear")
+    print("List is sorted!")
+    print(numList)
+    print(f"Attempts: {attempts}")
+        
+def main():
+    system("clear")
+    userChoice = input("How many numbers do you want to sort?: ")
+    numList = makeList(int(userChoice))
+    checkIfSorted(numList)
+    print(f"Process time: {timer() * 1.0E-9} s")
+
+if __name__ == "__main__":
+    main()
